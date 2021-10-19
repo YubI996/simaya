944196dbfa5a2f3ba5693486b7fb948cb3e5cfea
Sayid Fatich Sholachuddin Alayyubi <sayidyubi28@gmail.com>
2021-10-19 16:07:05 +0800

    tambah perhitungan

--- dashboard/member/rab.php
+++ dashboard/member/rab.php
@@ -1,33 +1,33 @@
-<?php include ('head.php'); ?>
+<?php include('head.php'); ?>
 
-        <div class="breadcome-area">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                        <div class="breadcome-list single-page-breadcome">
-                            <div class="row">
+<div class="breadcome-area">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="breadcome-list single-page-breadcome">
+                    <div class="row">
 
-                                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-0">
-                                    <div class="breadcome-heading">
-                                        
-                                    </div>
-                                </div>
-
-                                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                                    <ul class="breadcome-menu">
-                                        <li><a href="./">Home</a> <span class="bread-slash">/</span></li>
-                                        <li><span class="bread-blod">Dashboard</span></li>
-                                        <li style="padding: 0px 10px"></li>
-                                    </ul>
-                                </div>
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-0">
+                            <div class="breadcome-heading">
 
                             </div>
                         </div>
+
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <ul class="breadcome-menu">
+                                <li><a href="./">Home</a> <span class="bread-slash">/</span></li>
+                                <li><span class="bread-blod">Dashboard</span></li>
+                                <li style="padding: 0px 10px"></li>
+                            </ul>
+                        </div>
+
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
+</div>
 
 <?php
 
@@ -41,33 +41,35 @@ if ($allData->rowCount() < 1) {
     $pData = "0";
 } else {
     $pCount     = $allData->rowCount();
-    $pData      = $allData->fetchAll(PDO::FETCH_ASSOC);
+    $pData      = $allData->fetchAll(PDO::FETCH_ASSOC); //data proposal = $pData
 }
 
 ?>
 
-    <div class="product-status mg-tb-15" style="min-height: 67vh">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-12 col-md-12 hidden-sm hidden-xs">
-                    <div class="tab-content-details mg-b-30">
-                        <h2>Rancangan Anggaran Belanja <br>Dana Bantuan Keuangan Partai Politik</h2>
-                    </div>
-                </div>
-                <div class="hidden-lg hidden-md col-sm-12 col-xs-12">
-                    <div class="tab-content-details mg-b-30">
-                        <h2>Rancangan Anggaran Belanja <br>Dana Bantuan Keuangan <br>Partai Politik</h2>
-                    </div>
+<div class="product-status mg-tb-15" style="min-height: 67vh">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 hidden-sm hidden-xs">
+                <div class="tab-content-details mg-b-30">
+                    <h2>Rancangan Anggaran Belanja <br>Dana Bantuan Keuangan Partai Politik</h2>
                 </div>
             </div>
-            <div class="row"><!-- first row -->
-
-                <?php include ('../../lmnts/mem-dsh-ind.php'); ?>
-
-                <?php include ('../../lmnts/mem-dsh-indm.php'); ?>
-
-            </div><!-- end first row -->
+            <div class="hidden-lg hidden-md col-sm-12 col-xs-12">
+                <div class="tab-content-details mg-b-30">
+                    <h2>Rancangan Anggaran Belanja <br>Dana Bantuan Keuangan <br>Partai Politik</h2>
+                </div>
+            </div>
         </div>
-    </div>
+        <div class="row">
+            <!-- first row -->
 
-<?php include ('../../lmnts/ind-mdls.php'); include ('../footer.php'); ?>
+            <?php include('../../lmnts/mem-dsh-ind.php'); ?>
+
+            <?php include('../../lmnts/mem-dsh-indm.php'); ?>
+
+        </div><!-- end first row -->
+    </div>
+</div>
+
+<?php include('../../lmnts/ind-mdls.php');
+include('../footer.php'); ?>
